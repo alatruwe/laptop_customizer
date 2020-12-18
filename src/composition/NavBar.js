40fad5ab26e589d8e7285aac86bbf84902ec491d
adelinel <adeline.latruwe@gmail.com>
2020-12-18 09:29:19 -0800

    added NavBar component

---
+++ src/composition/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>ELF Computing | Laptops</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
