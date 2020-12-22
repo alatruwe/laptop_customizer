23de64940a1d91990bb86a57daaa7ef02c5b78e9
adelinel <adeline.latruwe@gmail.com>
2020-12-21 16:25:34 -0800

    updated state to display starting data

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ class App extends React.Component {
     this.state = {
       selected: {
         Processor: {
-          name: "",
-          cost: 0,
+          name: "17th Generation Intel Core HB (7 Core with donut spare)",
+          cost: 700,
         },
         "Operating System": {
-          name: "",
-          cost: 0,
+          name: "Ubuntu Linux 16.04",
+          cost: 200,
         },
         "Video Card": {
-          name: "",
-          cost: 0,
+          name: "Toyota Corolla 1.5v",
+          cost: 1150.98,
         },
         Display: {
-          name: "",
-          cost: 0,
+          name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+          cost: 1500,
         },
       },
     };
